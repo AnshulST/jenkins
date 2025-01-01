00659bd29180939e380da7f37839f24ffb3506c5
AnshulST <anshulnet1@gmail.com>
2025-01-01 14:32:58 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+package jenkins;
+
+public class Main {
+        public static void main(String[] args)
+        {
+            System.out.println("heello this is admin");
+            System.out.println("we are learning jenkins");
+            System.out.println("this is first commitcsadsadvds");
+            System.out.println("sdfs");
+            System.out.println("sdsada");
+
+        }
+
+    }
+
+
